109d988d39a48b6845b7c0a12fb8e16b3fe3ecb8
chensiwen <cikl777@163.com>
2024-12-24 10:13:14 +0800

    更新插件版本

--- frontend-vue/y9vue-dataCenter/src/App.vue
+++ frontend-vue/y9vue-dataCenter/src/App.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: your name
  * @Date: 2022-01-10 18:09:52
- * @LastEditTime: 2024-11-13 16:58:23
+ * @LastEditTime: 2024-12-24 10:04:49
  * @LastEditors: chensiwen cikl777@163.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /sz- team-frontend-9.6.x/y9vue-home/src/App.vue
 -->
 <script lang="ts" setup>
-    import zhCn from 'element-plus/lib/locale/lang/zh-cn';
-    import en from 'element-plus/lib/locale/lang/en';
+    import zhCn from 'element-plus/es/locale/lang/zh-cn';
+    import en from 'element-plus/es/locale/lang/en';
 </script>
 
 <template>
